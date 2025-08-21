7b2c6aa21077a1c1748206478678ca32c5e94148
Clement <clementaditya@gmail.com>
2025-08-21 15:06:14 +0800

    Echoes commands, terminates with bye

--- src/main/java/Udin.java
+++ src/main/java/Udin.java
@@ -1,10 +1,34 @@
+import java.util.Scanner;
+
 public class Udin {
+    private static final String line = "____________________________________________________________\n";
+
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________\n" +
+        System.out.println(Udin.line +
                 " Hello! I'm Udin!\n" +
                 " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________");
+                Udin.line);
+        Udin.read();
     }
+
+    private static void read() {
+        Scanner scanner = new Scanner(System.in);
+        String command = scanner.nextLine();
+        if (command.equals("bye")) {
+            Udin.bye();
+        } else {
+            Udin.echo(command);
+            Udin.read();
+        }
+
+    }
+
+    private static void echo(String command) {
+        System.out.println(Udin.line + " " + command + "\n" + Udin.line);
+    }
+
+    private static void bye() {
+        System.out.println(Udin.line + " Bye!\n" + Udin.line);
+    }
+
 }
