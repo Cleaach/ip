360c715974e6982013830544ac7be7abf68eb686
Clement <clementaditya@gmail.com>
2025-08-21 15:18:11 +0800

    Add and display list feature

--- src/main/java/Udin.java
+++ src/main/java/Udin.java
@@ -1,24 +1,29 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Udin {
     private static final String line = "____________________________________________________________\n";
+    private static ArrayList<String> text = new ArrayList<>();
 
     public static void main(String[] args) {
         System.out.println(Udin.line +
                 " Hello! I'm Udin!\n" +
                 " What can I do for you?\n" +
                 Udin.line);
-        Udin.read();
+        Udin.scan();
     }
 
-    private static void read() {
+    private static void scan() {
         Scanner scanner = new Scanner(System.in);
         String command = scanner.nextLine();
         if (command.equals("bye")) {
             Udin.bye();
+        } else if (command.equals("list")) {
+            Udin.list();
+            Udin.scan();
         } else {
-            Udin.echo(command);
-            Udin.read();
+            Udin.add(command);
+            Udin.scan();
         }
 
     }
@@ -31,4 +36,18 @@ public class Udin {
         System.out.println(Udin.line + " Bye!\n" + Udin.line);
     }
 
+    private static void add(String line) {
+        Udin.text.add(line);
+        System.out.println(Udin.line + " added: " + line + "\n" + Udin.line);
+    }
+
+    private static void list() {
+        int size = Udin.text.size();
+        System.out.println(Udin.line);
+        for (int i = 0; i < size; i++) {
+            int number = i + 1;
+            System.out.println(number + ". " + Udin.text.get(i));
+        }
+        System.out.println(Udin.line);
+    }
 }
