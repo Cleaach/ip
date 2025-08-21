c0f0f12e83b06b01c4bee2440a9b6aab9b544d07
Clement <clementaditya@gmail.com>
2025-08-21 15:34:53 +0800

    Mark and unmark

---
+++ src/main/java/Task.java
@@ -0,0 +1,25 @@
+public class Task {
+    private String title;
+    private boolean isDone;
+
+    public Task(String title) {
+        this.title = title;
+        this.isDone = false;
+    }
+
+    public String display() {
+        return (isDone ? "[X] " : "[ ] ") + this.getTitle(); // mark done task with X
+    }
+
+    public void mark() {
+        this.isDone = true;
+    }
+
+    public void unmark() {
+        this.isDone = false;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+}
--- src/main/java/Udin.java
+++ src/main/java/Udin.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Udin {
     private static final String line = "____________________________________________________________\n";
-    private static ArrayList<String> text = new ArrayList<>();
+    private static ArrayList<Task> todo = new ArrayList<>();
 
     public static void main(String[] args) {
         System.out.println(Udin.line +
@@ -21,6 +21,12 @@ public class Udin {
         } else if (command.equals("list")) {
             Udin.list();
             Udin.scan();
+        } else if (command.startsWith("mark ")) {
+            Udin.mark(Integer.parseInt(command.split(" ")[1]) - 1);
+            Udin.scan();
+        } else if (command.startsWith("unmark ")) {
+            Udin.unmark(Integer.parseInt(command.split(" ")[1]) - 1);
+            Udin.scan();
         } else {
             Udin.add(command);
             Udin.scan();
@@ -37,17 +43,27 @@ public class Udin {
     }
 
     private static void add(String line) {
-        Udin.text.add(line);
+        Udin.todo.add(new Task(line));
         System.out.println(Udin.line + " added: " + line + "\n" + Udin.line);
     }
 
     private static void list() {
-        int size = Udin.text.size();
-        System.out.println(Udin.line);
+        int size = Udin.todo.size();
+        System.out.println(Udin.line + "\n Your tasks:");
         for (int i = 0; i < size; i++) {
             int number = i + 1;
-            System.out.println(number + ". " + Udin.text.get(i));
+            System.out.println(" " + number + "." + Udin.todo.get(i).display());
         }
         System.out.println(Udin.line);
+    }
+
+    private static void mark(int index) {
+        Udin.todo.get(index).mark();
+        System.out.println(Udin.line + "Good boy! This task is all done:\n" + Udin.todo.get(index).display());
+    }
+
+    private static void unmark(int index) {
+        Udin.todo.get(index).unmark();
+        System.out.println(Udin.line + "This task was unmarked:\n" + Udin.todo.get(index).display());
     }
 }
